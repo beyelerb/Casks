cf6f2b1e2b66e0e911f03fcd2aeedadc3922992e
Bruce Beyeler <bruce.beyeler@connexta.com>
2018-10-09 13:14:37 -0700

    Fixed typo

--- passwordsafeswt.rb
+++ passwordsafeswt.rb
@@ -3,7 +3,7 @@ cask 'passwordsafeswt' do
     version '0.9.0-SNAPSHOT'
     sha256 :no_check
 
-    url "https://sourceforge.net/projects/pwsafe/files/jpwsafe/SWT-#{majversion}/PasswordSafeSWT-#{version}-mac64.dmg/download
+    url "https://sourceforge.net/projects/pwsafe/files/jpwsafe/SWT-#{majversion}/PasswordSafeSWT-#{version}-mac64.dmg/download"
     name 'PasswordSafeSWT'
     homepage 'https://jpwsafe.sourceforge.net'
 
