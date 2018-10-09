4537487787ed626c094eda49d0486325ad70e27b
Bruce Beyeler <bruce.beyeler@connexta.com>
2018-10-09 14:01:04 -0700

    Syntax errors fixed.

--- Casks/passwordsafeswt.rb
+++ Casks/passwordsafeswt.rb
@@ -1,9 +1,8 @@
 cask 'passwordsafeswt' do
-    majvsersion '0.9.RC'
     version '0.9.0-SNAPSHOT'
     sha256 :no_check
 
-    url "https://sourceforge.net/projects/pwsafe/files/jpwsafe/SWT-#{majversion}/PasswordSafeSWT-#{version}-mac64.dmg/download"
+    url "https://sourceforge.net/projects/pwsafe/files/jpwsafe/SWT-0.9.RC/PasswordSafeSWT-#{version}-mac64.dmg/download"
     name 'PasswordSafeSWT'
     homepage 'https://jpwsafe.sourceforge.net'
 
