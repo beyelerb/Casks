9ef1d76519faf6563625bbbdcaf79dfc14e8c0b5
Bruce Beyeler <bruce.beyeler@connexta.com>
2018-10-11 13:43:53 -0700

    Fixed quoting error.

--- Casks/securid.rb
+++ Casks/securid.rb
@@ -1,4 +1,4 @@
-cask 'rsasecurid' do
+cask 'securid' do
     version '4.1.2'
     sha256 '0dec8057a5f18e5e87b04c8263eabccdcc4b12396bbad905012fb1318ab29ebb'
 
@@ -7,7 +7,7 @@ cask 'rsasecurid' do
     homepage 'https://www.rsa.com/en-us/products/rsa-securid-suite/rsa-securid-access/securid-software-tokens.html'
     container :nested => 'RSASecurIDMac#{version}.dmg"
 
-    pkg 'RSASecurIDTokenAutoMac#{version.no_dots}x64.pkg'
+    pkg "RSASecurIDTokenAutoMac#{version.no_dots}x64.pkg"
     
     uninstall pkgutil: 'com.rsa.pkg.securidsoftwaretoken'
 end
