237b520178e0404a3c9aed6e728a8c2ee986b9fc
Bruce Beyeler <bruce.beyeler@connexta.com>
2019-07-16 22:37:02 -0700

    Updated audacity sha256

--- Casks/audacity.rb
+++ Casks/audacity.rb
@@ -1,6 +1,6 @@
 cask 'audacity' do
     version '2.3.2'
-    sha256 '0d2ea6de4f2cbd960abb6a6e020b51f6637423c072422596691960fcfae67206'
+    sha256 '9f3deba6c2bb8e4746304c3ebd19a0fa6398f4e529a7b2088794253cfe93983e'
 
     url "https://www.fosshub.com/Audacity.html?dwl=audacity-macos-#{version}.dmg"
     name 'Audacity'
