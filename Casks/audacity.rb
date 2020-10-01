5e24f7b4d7d8a781807299e5e6b495a1945d38f5
beyelerb <bruce.beyeler@connexta.com>
2020-10-01 09:11:27 -0700

    Updated version to 2.4.2

--- Casks/audacity.rb
+++ Casks/audacity.rb
@@ -1,5 +1,5 @@
 cask 'audacity' do
-    version '2.3.2'
+    version '2.4.2'
     sha256 '9f3deba6c2bb8e4746304c3ebd19a0fa6398f4e529a7b2088794253cfe93983e'
 
     url "https://www.fosshub.com/Audacity.html?dwl=audacity-macos-#{version}.dmg"
