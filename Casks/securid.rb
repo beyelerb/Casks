78348100e0e8ce97bc687acda11d6e2762262942
beyelerb <bruce.beyeler@connexta.com>
2020-10-01 09:16:06 -0700

    Update securid.rb

--- Casks/securid.rb
+++ Casks/securid.rb
@@ -4,7 +4,7 @@ cask 'securid' do
     # sha256 'e3d796f263cbdbc4a6c870931e469c64e8c8ffb14aa8ae68b4036cee0eeb0c04'
     sha256 '9987fe69ad212d65ae06b638a35229b481a5e3a4c86cc75f6574907940c5e658'
 
-    url "https://community.rsa.com/servlet/JiveServlet/download/62004-9-60039/RSASecurIDMac#{version.no_dots}.dmg.zip"
+    url "https://community.rsa.com/servlet/JiveServlet/download/62004-10-60039/RSASecurIDMac#{version.no_dots}.dmg.zip"
     name 'RSA SecurID'
     homepage 'https://www.rsa.com/en-us/products/rsa-securid-suite/rsa-securid-access/securid-software-tokens.html'
     container :nested => "RSASecurIDMac#{version.no_dots}.dmg"
